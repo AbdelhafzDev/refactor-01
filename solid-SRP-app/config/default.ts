2df7b5473c5ce1512d0ee0f093ea19307b1c672e
AbdelhafzDev <58186005+AbdelhafzDev@users.noreply.github.com>
2023-07-19 00:06:12 +0300

    Update default.ts

--- solid-SRP-app/config/default.ts
+++ solid-SRP-app/config/default.ts
@@ -1,3 +1,29 @@
 export default {
     origin: "http://localhost:8000"
 };
+
+function mapJsonToInterface<T>(json: any, iface: any): T {
+  const result: T = {} as T;  
+  
+  for (const key in iface.prototype) {
+    const value = json[key];
+    
+    if (typeof value !== 'undefined') {      
+      switch (typeof iface.prototype[key]) {
+        case 'string':          
+          result[key] = value as string;
+          break;               
+        case 'number':          
+          result[key] = value as number;          
+          break;   
+        case 'object':       
+          if (Array.isArray(iface.prototype[key])) {
+            result[key] = value as any[];  
+          }          
+          break;           
+      }
+    }   
+  }
+  
+  return result;
+}
