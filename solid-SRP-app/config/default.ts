f30af1f3c2af3def8c89c2c3b42484599276300f
AbdelhafzDev <58186005+AbdelhafzDev@users.noreply.github.com>
2023-07-19 00:33:30 +0300

    Update default.ts

--- solid-SRP-app/config/default.ts
+++ solid-SRP-app/config/default.ts
@@ -2,28 +2,31 @@ export default {
     origin: "http://localhost:8000"
 };
 
-function mapJsonToInterface<T>(json: any, iface: any): T {
-  const result: T = {} as T;  
-  
-  for (const key in iface.prototype) {
-    const value = json[key];
-    
-    if (typeof value !== 'undefined') {      
-      switch (typeof iface.prototype[key]) {
-        case 'string':          
-          result[key] = value as string;
-          break;               
-        case 'number':          
-          result[key] = value as number;          
-          break;   
-        case 'object':       
-          if (Array.isArray(iface.prototype[key])) {
-            result[key] = value as any[];  
-          }          
-          break;           
-      }
-    }   
-  }
+interface IProject {
+  id?: number;
+  name?: string;
+  description?: string;
+}
+
+interface IProjectResponse {
+  id?: number;
+  name?: string;
+  description?: string;
+  url?: string;
+  startDate?: string;
+  finishDate?: string;
+}
+
+const mapResponse = (response: IProjectResponse[]) => response.data.map((p) => ({
+  id: p.id,
+  name:p.name,
+  description: p.description,
+}));
+
+const response = await fetch(/* .. */);
+const data = await response.json();
+
+const projects: IProject[] = mapResponse(data);
   
   return result;
 }
